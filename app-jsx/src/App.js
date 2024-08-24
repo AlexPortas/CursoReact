9cf85377519db1af107c6dd38890106f8cfd0105
Alex <alexalonsoportas@gmail.com>
2024-08-24 22:22:15 +0200

    video 8

--- app-jsx/src/App.js
+++ app-jsx/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  // Definir estado para almacenar
+  const [resultado, setResultado]=useState(null);
+
   const titulo=<h1 className='centrar'>Aqui estamos...</h1>
-  const suma=sumando(83,18);
-  return <div><div>{titulo}</div><div>{suma}</div></div>
+
+  const botonPulsado=()=>{
+    const result=sumando(83,18);
+    setResultado(result);
+  }
+  return <div>
+      <button onClick={botonPulsado} style={{marginTop: "10px", marginLeft: "10px"}}>Pulsame</button>
+      <div>{titulo}</div>
+      <div>{resultado!=null && <h2>El resultado es {resultado}</h2>}
+      </div>
+    </div>
 }
 
 function sumando(a,b){

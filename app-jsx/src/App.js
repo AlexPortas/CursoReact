e5453923c665226c1da9a28284ecc8efc4dd0b36
Alex <alexalonsoportas@gmail.com>
2024-08-26 18:50:00 +0200

    useRef

--- app-jsx/src/App.js
+++ app-jsx/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, {useState} from "react";
+import Reaact, {useRef} from "react";
+import video from "./assets/video.mp4";
 
 function App() {
   // Definir estado para almacenar
@@ -11,10 +13,27 @@ function App() {
     const result=sumando(83,18);
     setResultado(result);
   }
+
+  const refVideo=useRef(null);
+  const videoPlay=()=>{
+    refVideo.current.play()
+  }
+  const videoPause=()=>{
+    refVideo.current.pause()
+  }
+
   return <div>
       <button onClick={botonPulsado} style={{marginTop: "10px", marginLeft: "10px"}}>Pulsame</button>
       <div>{titulo}</div>
-      <div>{resultado!=null && <h2>El resultado es {resultado}</h2>}
+      <div>{resultado!=null && <h2>El resultado es {resultado}</h2>}</div>
+      <div>
+        <video ref={refVideo}>
+          <source src={video} type='video/mp4'></source>
+        </video>
+        <div>
+          <button onClick={videoPlay}>Play</button>
+          <button onClick={videoPause}>Pause</button>
+        </div>
       </div>
     </div>
 }

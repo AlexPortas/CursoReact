e9aca7720b334203b956da139173e15c114726d3
Alex <alexalonsoportas@gmail.com>
2024-08-28 12:39:08 +0200

    funcionalidad conversor

--- conversor-jsx/src/App.js
+++ conversor-jsx/src/App.js
@@ -7,11 +7,16 @@ function App() {
   const resultRef=useRef();
 
   const convertir=()=>{
-    const eurosValor=eurosRef.current.value;
+    const eurosValor=parseFloat(eurosRef.current.value);
     const dolares=eurosValor*1.08;
-    resultRef.current.innerHTML=dolares
+    resultRef.current.innerHTML=dolares.toFixed(2)+" $  "
   }
-  return 
+  return  <div>
+    <h1>Conversor euros a dólares</h1>
+    <input className='centrar' type='text' ref={eurosRef}></input>
+    <button className='centrar' onClick={convertir}>Convertir</button>
+    <div className='centrar resultado' ref={resultRef}></div>
+  </div>
 }
 
 export default App;

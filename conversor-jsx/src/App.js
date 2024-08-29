67773e445f6cd39a134d428c6ce247dd97443fc1
Alex <alexalonsoportas@gmail.com>
2024-08-29 22:04:52 +0200

    meter api

--- conversor-jsx/src/App.js
+++ conversor-jsx/src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {useRef} from "react"
+import {useRef, useState, useEffect} from "react"
 
 function App() {
+  const [valorCamio, setValorCambio]=useState(null);
   const eurosRef=useRef();
   const resultRef=useRef();
-
+  useEffect(()=>{
+    const llamaAPI=async()=>{
+      try{
+          const respuesta=await fetch("https://v6.exchangerate-api.com/v6/3fcca9a31a526a04caf12b5d/latest/EUR");
+          const datos= await respuesta.json();
+          console.log(datos);
+          setValorCambio(datos.conversion_rates.USD)
+      }catch(e){
+        console.error(e);
+      }
+    }
+    llamaAPI();
+  },[]
+  );
   const convertir=()=>{
     const eurosValor=parseFloat(eurosRef.current.value);
-    const dolares=eurosValor*1.08;
+    const dolares=eurosValor*valorCamio;
     resultRef.current.innerHTML=dolares.toFixed(2)+" $  "
   }
   return  <div>
